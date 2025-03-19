878876d993960784c96be52233634c87381924f0
vaibhavsingh-0101 <vaibhavsingh010101@gmail.com>
2025-03-19 16:12:08 +0000

    Convertion od data type

---
+++ 01_basice/03_convertionOpration.js
@@ -0,0 +1,30 @@
+// let score = null
+
+
+
+// console.log(typeof score);
+
+
+
+// console.log(typeof (score));
+// let valueInNum =Number(score)
+
+// console.log( valueInNum)
+
+// "33" => 33
+//"33kdmk" =>NaN
+// true =>1 : fasle => 0
+
+let isLoggedIN = 1
+
+let boolenIsLoggedIN = Boolean(isLoggedIN)
+console.log(boolenIsLoggedIN)
+
+
+// 1 => ture : 0=>false
+// "" => false
+//"vaibhav"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+ console.log(typeof stringNumber)
