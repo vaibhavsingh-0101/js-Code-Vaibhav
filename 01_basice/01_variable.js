5dbf2225daf2bd990f27198e4ffbc589b6fd738d
vaibhavsingh-0101 <vaibhavsingh010101@gmail.com>
2025-03-19 15:54:33 +0000

    data type

---
+++ 01_basice/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 14453
+let accountEmail="Ram0101@gmail.com"
+var accountPassword = "12345"
+accountCity="Gurugram"
+
+let accountState;
+
+console.log(accountId)
+
+console.log(accountEmail)
+
+console.log(accountPassword)
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+console.log(accountCity)
+
+/* 
+prefer not to use var
+*/
+console.log(accountState)
+
+
+
---
+++ 01_basice/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // trate all js  code newer version
+
+// alert("hello")
+
+//we are using nodejs ,not browers
+
+console.log(3+3)// code readability should be high
+ console.log("vaibhav");
+
+
+ let name = "vaibhav"
+ let age = 16
+ let isLogedIn =true
+
+ // number => 2 to power of 53
+ //bigint
+ // strong =>""
+ //boolen => ture and false
+ // undefine
+ // null => stand alone value
+ // symbol=> unique
+ 
+
+ //Object
+
+    console
